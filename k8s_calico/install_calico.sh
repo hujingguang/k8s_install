160183a8421ab0123ed3b83c8653374e8adda5cd
hujingguang <hujingguangsa@gmail.com>
2019-11-29 19:56:34 +0800

    modify

--- k8s_calico/install_calico.sh
+++ k8s_calico/install_calico.sh
@@ -4,3 +4,5 @@
 #使用RBAC的话需要创建role. kubectl apply -f ./rbac.yaml 
 #etcd使用证书则需要 base64 etcd证书 : cat /etc/etcd/ca.pem |base64 |tr -d '\n'
 
+#calicoctl使用有两种方式：etcd datastore和api-server datastore 这个两个的区别是前者直接连接etcd操作policy，后者通过api-server操作etcd,且这两种方式数据不共用
+
